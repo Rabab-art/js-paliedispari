5981b4b32ea05571945654dbcd39eecdb8e7c211
Rabab-art <bouhamla.rabab@gmail.com>
2021-07-12 21:44:55 +0200

    First part- second exercise

---
+++ js/script2.js
@@ -0,0 +1,24 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer
+//  (usando una funzione).
+// // Sommiamo i due numeri
+// // Stabiliamo se la somma dei due numeri è pari o dispari 
+// (usando una funzione)
+// // Dichiariamo chi ha vinto.
+
+
+function random() {
+    var number = parseInt(prompt(Math.floor(Math.random() * 5) + 1));
+    number = parseInt(prompt(Math.floor(Math.random() * 5) + 1));
+
+    return number + number;
+
+};
+
+var number = random('Inserisci un numero')
+
+function somma(number, number) {
+    sum(numer + number);
+};
+
