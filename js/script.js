0ecbc3e5282df87f5104d26137ddc8efac346ed0
Rabab-art <bouhamla.rabab@gmail.com>
2021-07-12 20:49:01 +0200

    First exercise

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola 
+// inserita è palindroma.
+
+
+
+
+
+// function isPalindrome(str) {
+
+
+//     var len = str.length;
+//     var mid = Math.floor(len / 2);
+
+//     for (var i = 0; i < mid; i++) {
+//         if (str[i] !== str[len - 1 - i]) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+
+// }
+
+
+
+function isPalindrome(s) {
+    return s == s.split("").reverse().join("");
+    if (s === s) {
+        return true
+    }
+    return false;
+
+};
+
+
+var palindrome = isPalindrome(prompt('Inserisci una parola', ''));
+console.log(palindrome);
