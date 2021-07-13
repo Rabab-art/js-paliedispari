f94045acb69de12a84b19f37dfa4e61619b4584f
Rabab-art <bouhamla.rabab@gmail.com>
2021-07-13 16:38:51 +0200

    Dispari exercise

--- js/script2.js
+++ js/script2.js
@@ -8,17 +8,37 @@
 // // Dichiariamo chi ha vinto.
 
 
-function random() {
-    var number = parseInt(prompt(Math.floor(Math.random() * 5) + 1));
-    number = parseInt(prompt(Math.floor(Math.random() * 5) + 1));
+// function random() {
+//     var number = parseInt(prompt(Math.floor(Math.random() * 5) + 1));
+//     number = parseInt(prompt(Math.floor(Math.random() * 5) + 1));
 
-    return number + number;
+//     return number + number;
 
+// };
+
+// var number = random('Inserisci un numero')
+
+// function somma(number, number) {
+//     sum(numer + number);
+// };
+
+var userChoice = prompt('scegli "pari"o"dispari"', 'pari');
+
+// VALIDATION
+while (userChoice.toLowerCase().trim() !== 'pari' &&
+    userChoice.toLowerCase().trim() !== 'dispari');
+
+userChoice = prompt('scegli "pari"o"dispari"', 'pari');
+
+var userNumber = prompt('Scegli un numero da 1 a 5', 'pari');
+
+// VALIDATION
+
+while (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
+    userNumber = prompt('scegli "pari"o"dispari"', 'pari');
 };
+//  GENERO UN NUMERO
+var cpuNumber = Math.floor(Math.random() * (5 - 1 + 1) - 1);
+var sum = parseInt(userNumber) + cpuNumber;
 
-var number = random('Inserisci un numero')
-
-function somma(number, number) {
-    sum(numer + number);
-};
-
+console.log(sum);
