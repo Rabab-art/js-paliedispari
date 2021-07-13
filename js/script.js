90a02b0336b97e3c4b7d5a0bd8096363a3640863
Rabab-art <bouhamla.rabab@gmail.com>
2021-07-13 15:41:42 +0200

    Palindrome exercise

--- js/script.js
+++ js/script.js
@@ -7,33 +7,34 @@
 
 
 
-// function isPalindrome(str) {
+function isPalindrome(str) {
 
 
-//     var len = str.length;
-//     var mid = Math.floor(len / 2);
+    var len = str.length;
+    var mid = Math.floor(len / 2);
 
-//     for (var i = 0; i < mid; i++) {
-//         if (str[i] !== str[len - 1 - i]) {
-//             return false;
-//         }
-//     }
-
-//     return true;
-
-// }
-
-
-
-function isPalindrome(s) {
-    return s == s.split("").reverse().join("");
-    if (s === s) {
-        return true
+    for (var i = 0; i < mid; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
     }
-    return false;
 
-};
+    return true;
+
+}
 
 
-var palindrome = isPalindrome(prompt('Inserisci una parola', ''));
+
+// function isPalindrome(s) {
+//     var s = s.split("").reverse().join("");
+//     s = 's';
+//     if (s === s) {
+//         return true
+//     }
+//     return false;
+
+// };
+
+
+var palindrome = isPalindrome(prompt('Inserisci una parola', 'giorgio'));
 console.log(palindrome);
